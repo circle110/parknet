62b902754f8a03d17eb25cad48d2d38007d8791b
Mike <mkentsmith@gmail.com>
2013-08-03 21:34:09 -0500

    spelling error

--- app/models/registration_basket.rb
+++ app/models/registration_basket.rb
@@ -1,6 +1,6 @@
 class RegistrationBasket < ActiveRecord::Base
 	belongs_to :agency
-	belongd_to :customer
+	belongs_to :customer
 	has_many :registration_basket_line_items
 	
 	attr_accessible :agency_id, :customer_id, :registration_basket_line_item
